814dab4e560ae449293ba148821050b4bf490697
Frize <voisine.kevin2@gmail.com>
2025-06-05 16:47:12 +0200

    [✨ feat] remplacement des composants Navigation et Footer par NavigationBar et FooterBar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import useEruda from './composables/eruda'
-import Navigation from './components/layout/Navigation.vue'
-import Footer from './components/layout/Footer.vue'
+import NavigationBar from './components/layout/NavigationBar.vue'
+import FooterBar from './components/layout/FooterBar.vue'
 useEruda()
 </script>
 
 <template>
-  <Navigation />
+  <NavigationBar />
   <div class="p-4 flex-1">
     <RouterView />
   </div>
-  <Footer />
+  <FooterBar />
 </template>
